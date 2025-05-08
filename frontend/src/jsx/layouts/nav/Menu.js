f3f4ac85434479afeee0af752735033f1cafebb6
Sivasundhar16 <vssiva18@gmail.com>
2025-05-08 12:40:43 +0530

    changed file name admin to frontend and fixed route - when login it would directly go to details page

---
+++ frontend/src/jsx/layouts/nav/Menu.js
@@ -0,0 +1,24 @@
+export const MenuList = [
+  {
+    title: "DOM Users",
+    classChange: "mm-collapse",
+    iconStyle: <i className="flaticon-093-waving" />,
+    content: [
+      {
+        title: "Company",
+        to: "job-list",
+      },
+    ],
+  },
+  {
+    title: "Admin",
+    classsChange: "mm-collapse",
+    iconStyle: <i className="flaticon-025-dashboard"></i>,
+    content: [
+      {
+        title: "Add Company",
+        to: "dashboard",
+      },
+    ],
+  },
+];
